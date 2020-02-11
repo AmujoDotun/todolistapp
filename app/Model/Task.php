85aa3f1f16208f9213e8a5455263be0687fb65e4
AmujoDotun <samuelamujo@gmail.com>
2020-02-11 15:45:26 +0100

    The date formatting of the task is not not cool, its like this 2/22/2020 so I make the date formatting more cool to something like this Oct 1, 2020

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -2,12 +2,18 @@
 
 namespace App\Model;
 
+use App\Http\Traits\TasksTrait;
 use App\Http\Traits\TimestampsTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use TimestampsTrait;
+    use TasksTrait;
+
+    public function getDate(){
+        return ['created_at', 'updated_at', 'due_date'];
+    }
 
     //define the table
     protected $table = 'tasks';
